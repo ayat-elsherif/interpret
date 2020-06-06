d1828cdcde6fcdcf5ae3c6e300c46bc214d6d11c
Ayat Elsherif <ayat_elsherif@yahoo.com>
2020-06-06 21:26:59 +0200

    add offcanvas facility and ajust its code

--- js/script.js
+++ js/script.js
@@ -22,7 +22,25 @@ $(function(){
       scrollToUp.click(function(){
         $('html,body').animate({scrollTop:0},500);
       });
-    
+
+    /**************offcanvas *************/
+    $("[data-trigger]").on("click", function(e){
+      e.preventDefault();
+      e.stopPropagation();
+      var offcanvas_id =  $(this).attr('data-trigger');
+      $(offcanvas_id).toggleClass("showCanvas");
+      $('body').toggleClass("offcanvas-active");
+      $(".screen-overlay").toggleClass("showCanvas");
+  }); 
+  
+  $(".btn-close, .screen-overlay, #navbarSupportedContent .nav-item").click(function(e){
+      $(".screen-overlay").removeClass("showCanvas");
+      $(".mobile-offcanvas").removeClass("showCanvas");
+      $("body").removeClass("offcanvas-active");
+  }); 
+/**************offcanvas *************/
+
+
     $('#homeBlog').owlCarousel({
         loop:true,
         dots:false,
@@ -68,9 +86,9 @@ $(function(){
         html:true
     });
 
-    $('#navbarSupportedContent .nav-item').click(function(){
-      $('#navbarSupportedContent').removeClass('show');
-    });
+    // $('#navbarSupportedContent .nav-item').click(function(){
+    //   $('#navbarSupportedContent').removeClass('show');
+    // });
 
     new WOW().init();
 
